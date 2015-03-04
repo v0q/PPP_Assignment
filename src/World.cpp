6633e6894b856081782fab3c970859baca431b09
v0q <teemu.v0q.lindborg@gmail.com>
2015-03-04 18:00:36 +0000

    Removed an unnecessary function

--- src/World.cpp
+++ src/World.cpp
@@ -122,39 +122,3 @@ void World::tSphere(int _i) const
     subd(icosHedron[k][0], icosHedron[k][1], icosHedron[k][2], _i);
   }
 }
-
-void World::sphere(float _radius) const
-{
-  //  Sphere code based on a function Written by Paul Bourke.
-  //  http://astronomy.swin.edu.au/~pbourke/opengl/sphere/
-  // the next part of the code calculates the P,N,UV of the sphere for tri_strips
-
-  float theta1 = 0.0;
-  float theta2 = 0.0;
-  float theta3 = 0.0;
-  Vec4 normal;
-  Vec4 vertex;
-
-  glBegin(GL_TRIANGLE_STRIP);
-  // now fill in a vertData structure and add to the data list for our sphere
-  for( int i = 0; i < PRECISION/2; ++i )
-  {
-   theta1 = i * TWO_PI / PRECISION - PI2;
-   theta2 = (i + 1) * TWO_PI / PRECISION - PI2;
-
-   for( int j = 0; j <= PRECISION; ++j )
-   {
-     theta3 = j * TWO_PI / PRECISION;
-     normal.set(cosf(theta2) * cosf(theta3),sinf(theta2),cosf(theta2) * sinf(theta3));
-     vertex = normal * _radius;
-     normal.normalGL();
-     vertex.vertexGL();
-     // vert / normal 2
-     normal.set(cosf(theta1) * cosf(theta3),sinf(theta1),cosf(theta1) * sinf(theta3));
-     vertex = normal * _radius;
-     normal.normalGL();
-     vertex.vertexGL();
-    } // end inner loop
-  }// end outer loop
-  glEnd();
-}
